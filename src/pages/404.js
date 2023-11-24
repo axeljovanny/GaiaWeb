9afb5b5f55d3d1bd81c9e9d36265455ed5754dff
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2023-11-24 12:35:31 -0600

    fix SEO component 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,21 @@
-import React from "react";
+import * as React from "react"
 import Layout from "../components/layout";
-import SEO from "../components/seo";
 import { Helmet } from "react-helmet";
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components";
+import { Link } from "gatsby"
+import { colors } from "../utils/const";
+
+const pageStyles = {
+    display: "flex",
+    width: "100vw",
+    height: "60vh",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
 
 const canonicalUrl = "https://gaiaevolutionspaandsalon.com/404";
 
@@ -15,12 +27,19 @@ const NotFoundPage = () => (
         </Helmet>
         <Layout>
             <Navbar siteTitle="index" />
-            <SEO title="404: Not found" />
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <main style={pageStyles}>
+                <h1 >Pagína no encontrada</h1>
+                <p >
+                    Lo sentimos mano 😔, la pagína a la que quieres entrar no existe, pero puedes ir a nuetro Inicio.
+                </p>
+                <p><Link to="/" style={{color:colors.accentBlue}}>Visitar Inicio</Link>.</p>
+                
+            </main>
             <Footer></Footer>
         </Layout>
     </>
 );
 
 export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>
